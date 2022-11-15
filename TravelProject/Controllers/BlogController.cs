aa7225266b8502f47b3d1d04e21dd0ad236b84f0
Muhammed Can GEZGİN <93928479+Cangezgin1@users.noreply.github.com>
2022-11-15 15:02:53 +0300

    Düzenlemeler...

---
+++ TravelProject/Controllers/BlogController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelProject.Models.Sınıflar;
+
+namespace TravelProject.Controllers
+{
+    public class BlogController : Controller
+    {
+
+        Context c = new Context();
+
+        public ActionResult Index()
+        {
+            var values = c.Blogs.ToList();
+            return View(values);
+        }
+
+        public ActionResult BlogDetay(int id)
+        {
+            return View();
+        }
+    }
+}
