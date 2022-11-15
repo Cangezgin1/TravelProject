bb50595b4384953eb14523416073cad9df247746
Muhammed Can GEZGİN <93928479+Cangezgin1@users.noreply.github.com>
2022-11-15 11:25:02 +0300

    Add project files.

---
+++ TravelProject/Models/Sınıflar/Adres.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelProject.Models.Sınıflar
+{
+    public class Adres
+    {
+        public int ID { get; set; }
+        public string Baslik { get; set; }
+        public string Aciklama { get; set; }
+        public string AcikAdres { get; set; }
+        public string Mail { get; set; }
+        public string Telefon { get; set; }
+        public string Konum { get; set; }
+    }
+}
